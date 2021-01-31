b8ea5c6f12b669acc679af26bc83040c308dc7a7
buggaisthehero <buggathehero@gmail.com>
2021-01-31 17:23:59 +0200

    Added Transactions page class

---
+++ src/main/java/pageObjects/Transactions.java
@@ -0,0 +1,15 @@
+package pageObjects;
+
+
+import enummerables.Enums.LocatorType;
+import helperObjects.TestBase;
+import utilities.SeleniumDriver;
+
+public class Transactions extends TestBase {
+
+	public Transactions(SeleniumDriver seleniumDriver) 
+    {
+    	super(seleniumDriver);
+    }
+	
+}
